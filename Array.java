f3acae01355c778fe96d6dbc5b475a6174d70e2c
Indhureddy05 <kassaindhureddy2006@gmail.com>
2026-03-11 14:45:29 +0530

    Implement array operations in Array.java
    Add basic array manipulation logic and examples to Array.java, including initialization, element access, and modification methods.

---
+++ Array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Array
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] a = new int[n][3];
+
+		for(int i=0; i<n; i++)
+			for(int j=0; j<3; j++)
+			    a[i][j] = sc.nextInt();
+
+		for(int i=0; i<n; i++) 
+		{
+			for(int j=0; j<3; j++)
+			    System.out.print(a[i][j] + " ");
+			
+			System.out.println();
+		}
+
+	}
+}
